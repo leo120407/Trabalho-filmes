309bba8f744166b713ce696855dc8d5c7ceefed8
Lewou <leonardoborim@gmail.com>
2026-06-05 20:30:34 -0300

    modified:   script.js

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-const API_KEY = process.env.TMDB_API_KEY;
-
 async function teste() {
-    let url = 'https://api.themoviedb.org/3/movie/11';
-    let movie_data = await fetch(url, {
-        method: 'GET',
-        headers: {
-            'Authorization': `Bearer ${API_KEY}`
-        }
-    })
+     dados = await fetch('/api/buscar-dados.js')
+     console.log(dados);
 }
