c2e4c55f07202f39eb4378a7248ff89a7683776d
lewou12 <78835989+leo120407@users.noreply.github.com>
2026-06-12 20:08:48 -0300

    Clean up comments in buscar-dados.js
    Removed comments explaining the steps in the code.

--- api/buscar-dados.js
+++ api/buscar-dados.js
@@ -1,19 +1,15 @@
 // api/filmes.js
 export default async function handler(request, response) {
-  // 1. Pega o Token seguro das variáveis de ambiente da Vercel
   const TMDB_TOKEN = process.env.TMDB_API_KEY;
 
   const {query, language = "pt-br"} = request.query;
 
-  // 2. Define qual rota do TMDB você quer chamar (ex: filmes populares)
   const url = `https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(query)}&language=${language}`;
 
-  // 3. Configura a autenticação que o TMDB exige
   const options = {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      // Aqui enviamos o token de forma camuflada pelo servidor
       Authorization: `Bearer ${TMDB_TOKEN}` 
     }
   };
@@ -22,7 +18,6 @@ export default async function handler(request, response) {
     const apiResponse = await fetch(url, options);
     const data = await apiResponse.json();
 
-    // 4. Devolve a lista de filmes mastigada para o seu HTML/JS
     return response.status(200).json(data);
   } catch (error) {
     return response.status(500).json({ error: 'Erro ao buscar filmes do TMDB' });
